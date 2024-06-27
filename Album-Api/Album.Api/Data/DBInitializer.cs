89e7589cd55894da7372ab06edfe4548b68d86da
packetphysicist <dpnales@gmail.com>
2024-06-27 17:59:49 +0200

    fixed img links in seeder

--- Album-Api/Album.Api/Data/DBInitializer.cs
+++ Album-Api/Album.Api/Data/DBInitializer.cs
@@ -14,8 +14,8 @@ namespace Album.Api.Data
 
          var albums = new AlbumModel[]
          {
-            new AlbumModel { Id = 1, Name="Dog of Wisdom", Artist="Joe", ImageUrl="https://www.google.com/imgres?q=dog%20of%20wisdom&imgurl=https%3A%2F%2Fm.media-amazon.com%2Fimages%2FM%2FMV5BMTQwYmM2OWQtNWZmOS00OWZhLWE1YzYtMWQyYTk3MzhiOGYzXkEyXkFqcGdeQXVyMjQxNzM0MjI%40._V1_.jpg&imgrefurl=https%3A%2F%2Fwww.imdb.com%2Ftitle%2Ftt10208550%2F&docid=tSkrLs0QVJ0GiM&tbnid=suPM4q5IS7Wa_M&vet=12ahUKEwi41fn88vuGAxWMgP0HHfDcCLAQM3oECBUQAA..i&w=1280&h=721&hcb=2&ved=2ahUKEwi41fn88vuGAxWMgP0HHfDcCLAQM3oECBUQAA" },
-            new AlbumModel { Id = 2, Name="Hond van Wijsheid", Artist="Johan", ImageUrl="https://www.google.com/imgres?q=dog%20of%20wisdom&imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FHKCWMq-Poxo%2Fsddefault.jpg&imgrefurl=https%3A%2F%2Fm.youtube.com%2Fwatch%3Fv%3DHKCWMq-Poxo&docid=v3q6xLAICNK8ZM&tbnid=8VGeTdWiIIWbOM&vet=12ahUKEwi41fn88vuGAxWMgP0HHfDcCLAQM3oECGIQAA..i&w=640&h=480&hcb=2&ved=2ahUKEwi41fn88vuGAxWMgP0HHfDcCLAQM3oECGIQAA" }
+            new AlbumModel { Id = 1, Name="Dog of Wisdom", Artist="Joe", ImageUrl="https://m.media-amazon.com/images/M/MV5BMTQwYmM2OWQtNWZmOS00OWZhLWE1YzYtMWQyYTk3MzhiOGYzXkEyXkFqcGdeQXVyMjQxNzM0MjI@._V1_FMjpg_UX1000_.jpg" },
+            new AlbumModel { Id = 2, Name="Hond van Wijsheid", Artist="Johan", ImageUrl="https://cdn.britannica.com/13/234213-050-45F47984/dachshund-dog.jpg" }
          };
 
          foreach (AlbumModel album in albums) {
